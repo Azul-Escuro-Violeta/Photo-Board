d1b1a5b838ee2915323b0bcc37311be13492228a
Danielle Soares <silvasoaresdanielle2@gmail.com>
2023-10-11 23:32:44 -0300

    commit initial

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container_text}>
+        <Text style={styles.text}>Bem Vindo!</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +22,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: 20,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E1E1E',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%'
   },
+  
+  container_text: {
+    width: '70%'
+  },
+
+  text: {
+    color: 'white',
+    marginBottom: 10,
+    fontSize: '30px',
+
+  },
+
+  input: {
+    alignItems: 'center',
+    width: '70%',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 20,
+  },
+
 });
