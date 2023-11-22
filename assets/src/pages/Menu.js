cd2b69712fc19157dbe5def7c41f66224c22c972
Danielle Soares <silvasoaresdanielle2@gmail.com>
2023-11-22 18:10:04 -0300

    calendário do menu definitivo

--- assets/src/pages/Menu.js
+++ assets/src/pages/Menu.js
@@ -1,89 +1,166 @@
-import React from 'react';
-import {Text, View, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
-import {AntDesign} from '@expo/vector-icons'; 
-import {useFonts} from 'expo-font';
+import React, { useState, useRef } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  TouchableWithoutFeedback,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import moment from 'moment';
+import Swiper from 'react-native-swiper';
 import Header from '../../components/Header/index.js'
-import { useNavigation } from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {AntDesign} from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'; 
 
+const { width } = Dimensions.get('window');
 
 export default function Menu() {
   const navigation = useNavigation();
-  const [fontsLoaded] = useFonts({
-    'Poppins-Regular': require('../../fonts/Poppins-Regular.ttf'),
-    'Poppins-Bold': require('../../fonts/Poppins-Bold.ttf'),
-    'Poppins-Medium': require('../../fonts/Poppins-Medium.ttf')
-  });
+  
+  const swiper = useRef();
+  const [value, setValue] = useState(new Date());
+  const [week, setWeek] = useState(0);
 
-  if (!fontsLoaded) {
-    return undefined;
+  function switchDay (number) {
+
+    let dayWeek = 'N'
+
+    switch (number) {
+      case 0:
+        dayWeek = 'D';
+        break;
+      case 1:
+        dayWeek = 'S';
+        break;
+      case 2:
+        dayWeek = 'T';
+        break;
+      case 3:
+        dayWeek = 'Q';
+        break;
+      case 4:
+        dayWeek = 'Q';
+        break;
+      case 5:
+        dayWeek = 'S';
+        break;
+      case 6:
+        dayWeek = 'S';
+        break;
+    }
+
+    return dayWeek
   }
+
+  const weeks = React.useMemo(() => {
+    const start = moment().add(week, 'weeks').startOf('week');
+
+    return [-1, 0, 1].map(adj => {
+      return Array.from({ length: 7 }).map((_, index) => {
+        const date = moment(start).add(adj, 'week').add(index, 'day');
+   
+        return {
+
+          weekday: switchDay (parseInt(date.format('d'))),
+          date: date.toDate(),
+        };
+      });
+    });
+  }, [week]);
 
   return (
     <>
-    <Header/>
-    <View style={styles.container}>
+    <View style={styles.viewp}>
+      <Header/>
       <TouchableOpacity
-      style={styles.calendar_button}
-      underlayColor="lightblue"
-      onPress={() => navigation.navigate('Calendar')}
+        style={styles.calendar_button}
+        underlayColor="lightblue"
+        onPress={() => navigation.navigate('Calendar')}
       >
-        <Text style={styles.calendar_text}>Acessar Calendário</Text>
-        <AntDesign name="right" size={24} color="white" />
+          <Text style={styles.calendar_text}>Acessar Calendário</Text>
+          <AntDesign name="right" size={24} color="white" />
       </TouchableOpacity>
-      <View style={styles.calendar_container}>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>D</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
+      <View style={styles.swiperContainer}>
+          <View style={styles.picker}>
+            <Swiper
+              index={1}
+              ref={swiper}
+              loop={false}
+              showsPagination={false}
+              onIndexChanged={ind => {
+                if (ind === 1) {
+                  return;
+                }
+                setTimeout(() => {
+                  const newIndex = ind - 1;
+                  const newWeek = week + newIndex;
+                  setWeek(newWeek);
+                  setValue(moment(value).add(newIndex, 'week').toDate());
+                  swiper.current.scrollTo(1, false);
+                }, 100);
+              }}>
+              {weeks.map((dates, index) => (
+                <View
+                  style={[styles.itemRow, { paddingHorizontal: 16 }]}
+                  key={index}>
+                  {dates.map((item, dateIndex) => {
+                    const isActive =
+                      value.toDateString() === item.date.toDateString();
+                    return (
+                      <TouchableWithoutFeedback
+                        key={dateIndex}
+                        onPress={() => setValue(item.date)}>
+                        <View
+                          style={
+                            styles.item}>
+                          <Text
+                            style={[
+                              styles.itemWeekday,
+                              isActive && { color: '#fff' },
+                            ]}>
+                            {item.weekday}
+                          </Text>
+                          <View style={[
+                                styles.itemDateContainer,
+                                isActive && {backgroundColor: '#fff' },
+                              ]}>
+                            <Text
+                              style={[
+                                styles.itemDate,
+                                isActive && {color: '#000' },
+                              ]}>
+                              {item.date.getDate()}
+                            </Text>
+                          </View>
+                          <Text 
+                            style={[
+                            styles.itemtoday,
+                            isActive && { color: '#fff', display: 'flex' },
+                            ]}>
+                              Hoje
+                          </Text>
+                        </View>
+                      </TouchableWithoutFeedback>
+                    );
+                  })}
+                </View>
+              ))}
+            </Swiper>
           </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>S</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>T</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>Q</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>Q</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>S</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.dayName}>S</Text>
-          <View style={styles.dayNumber_container}>
-            <Text style={styles.dayNumber}>14</Text>
-          </View>
-        </View>
       </View>
-    </View>
+      </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  
+  viewp: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    flexDirection: 'column',
     backgroundColor: '#1E1E1E',
   },
 
@@ -91,16 +168,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontFamily:"Poppins-Medium",
     fontSize: 16,
-  },
-
-  calendar_container: {
-    marginTop: 30,
-    height: 80,
-    width: "100%",
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10,
   },
 
   calendar_button: {
@@ -111,32 +178,108 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  day: {
+  placeholderInset: {
+    borderWidth: 4,
+    borderColor: '#e5e7eb',
+    borderStyle: 'dashed',
+    borderRadius: 9,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#007aff',
+    borderColor: '#007aff',
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  swiperContainer: {
+    paddingVertical: 24,
+  },
+  picker: {
+    height: 'auto',
+    maxHeight: 120,
+    paddingVertical: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitle: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#999999',
+    marginBottom: 12,
+  },
+  header: {
+    paddingHorizontal: 16,
+  },
+
+  content: {
+    paddingHorizontal: 16,
+  },
+
+  footer: {
+    marginTop: 'auto',
+    paddingHorizontal: 16,
+  },
+
+  itemRow: {
+    width: width,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    marginHorizontal: -4,
+  
+  },
+
+  item: {
+    flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'space-between',
-    height: '100%',
+    height: 50,
+    
   },
-
-  dayName: {
+  
+  itemWeekday: {
+    fontSize: 20,
+    fontWeight: '500',
     color: '#297447',
-    fontSize: 32,
+    marginBottom: 4,
     fontFamily: 'Poppins-Bold',
   },
 
-  dayNumber_container: {
-    backgroundColor: '#262626',
-    width: 40,
-    height: 40,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',    
+  itemDate: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: 'white',
+    
   },
 
-  dayNumber: {
-    color: 'white',
-    borderRadius: '100%',
-    fontSize: 20,
-    fontFamily: 'Poppins-Medium',
+  itemDateContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#262626',
+    height: 42,
+    width: 42,
+    borderRadius: '50%',
+  },
+
+  itemtoday: {
+    marginTop: 3,
+    fontSize: 14,
+    fontFamily: 'Poppins-Regular',
+    display: 'none',
   },
 });
