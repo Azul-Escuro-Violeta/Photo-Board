661870367df7bea9899e325b7ac8f29c066eb9c2
pfzzs <113647882+pfzzs@users.noreply.github.com>
2023-12-14 00:25:22 -0300

    fix
    erros de sintaxe na propriedade "BorderRadius" resolvidos

---
+++ Camera.js
@@ -0,0 +1,133 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {Button, Text, View, TextInput, StyleSheet, TouchableOpacity, Modal, Image} from 'react-native';
+import {AntDesign} from '@expo/vector-icons'; 
+import Header from '../../components/Header/index.js'
+import { Camera, CameraType } from 'expo-camera';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function Calendar() {
+  const camRef = useRef(null);
+  const [type, setType] = useState (Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [open, setOpen] = useState(null);
+
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission (status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission == null) {
+    return <View/>
+  }
+
+  if (hasPermission === false) {
+    return <Text> Acesso Negado! </Text>;
+  }
+  
+  async function takePicture() {
+    if(camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri)
+      setOpen(true)
+      console.log(data);
+    } 
+  }
+
+
+  return (
+    <>
+    <Header/>
+    <View style={styles.container}>
+      <Camera
+      style={{flex: 1}}
+      type={type}
+      ref = {camRef}
+      >
+        <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'center',}}>
+        <TouchableOpacity
+            style={styles.return_button}
+            underlayColor="lightblue"
+          >
+            <AntDesign name="left" size={30} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+          style={{
+            position: 'absolute',
+            bottom: 30,
+            left: 30,
+          }}
+          
+          onPress={ () => {
+            setType(
+              type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+            );
+          }}>
+            <Ionicons name="camera-reverse-outline" size={60} color="black"  style={{marginBottom: 13, color: 'white'}}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+          style={{
+            JustifyContent: 'center',
+            alignItems: 'center',
+            bottom: -685,
+          }}
+          
+          onPress={takePicture}>
+            <View style={{width: 65, height: 65, backgroundColor: '#297447', borderRadius: 100}}
+            ></View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {
+        capturedPhoto && 
+        <Modal 
+        animationType="slide"
+        transparent={false}
+        visible= {open}
+        >
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20, backgroundColor: '#1E1E1E'}}>
+            <TouchableOpacity style={{margin:10, alignItems: 'center', justifyContent:'center', flexDirection: 'row'}} onPress={()=> setOpen(false)}>
+              <Text style={{marginRight:5, fontSize: 24, fontFamily: 'Poppins-Regular', color: 'white'}}>Fechar</Text>
+              <Ionicons name="md-close" size={40} color="red" />
+            </TouchableOpacity>
+            <Image
+              style={{width: '100%', height: 500, borderRadius: 20}}
+              source={{uri: capturedPhoto}}
+            />
+          </View>
+        </Modal>
+      }
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#297447',
+  },
+
+  return_icon_container: {
+    width: '100%',
+    marginLeft: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+},
+
+return_button: {
+    position: 'absolute',
+    top: 30,
+    left: 30,
+},
+
+});
---
+++ Menu.js
@@ -0,0 +1,384 @@
+import React, { useState, useRef } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  TouchableWithoutFeedback,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import moment from 'moment';
+import Swiper from 'react-native-swiper';
+import Header from '../../components/Header/index.js'
+import {useFonts} from 'expo-font';
+import {AntDesign} from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'; 
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
+const { width } = Dimensions.get('window');
+
+export default function Menu() {
+  const navigation = useNavigation();
+  
+  const swiper = useRef();
+  const [value, setValue] = useState(new Date());
+  const [week, setWeek] = useState(0);
+
+  function switchDay (number) {
+
+    let dayWeek = 'N'
+
+    switch (number) {
+      case 0:
+        dayWeek = 'D';
+        break;
+      case 1:
+        dayWeek = 'S';
+        break;
+      case 2:
+        dayWeek = 'T';
+        break;
+      case 3:
+        dayWeek = 'Q';
+        break;
+      case 4:
+        dayWeek = 'Q';
+        break;
+      case 5:
+        dayWeek = 'S';
+        break;
+      case 6:
+        dayWeek = 'S';
+        break;
+    }
+
+    return dayWeek
+  }
+
+  const weeks = React.useMemo(() => {
+    const start = moment().add(week, 'weeks').startOf('week');
+
+    return [-1, 0, 1].map(adj => {
+      return Array.from({ length: 7 }).map((_, index) => {
+        const date = moment(start).add(adj, 'week').add(index, 'day');
+   
+        return {
+
+          weekday: switchDay (parseInt(date.format('d'))),
+          date: date.toDate(),
+        };
+      });
+    });
+  }, [week]);
+
+  return (
+    <>
+    <View style={styles.viewp}>
+      <Header/>
+      <TouchableOpacity
+        style={styles.calendar_button}
+        underlayColor="lightblue"
+        onPress={() => navigation.navigate('Calendar')}
+      >
+          <Text style={styles.calendar_text}>Acessar Calendário</Text>
+          <AntDesign name="right" size={24} color="white" />
+      </TouchableOpacity>
+      <View style={styles.swiperContainer}>
+          <View style={styles.picker}>
+            <Swiper
+              index={1}
+              ref={swiper}
+              loop={false}
+              showsPagination={false}
+              onIndexChanged={ind => {
+                if (ind === 1) {
+                  return;
+                }
+                setTimeout(() => {
+                  const newIndex = ind - 1;
+                  const newWeek = week + newIndex;
+                  setWeek(newWeek);
+                  setValue(moment(value).add(newIndex, 'week').toDate());
+                  swiper.current.scrollTo(1, false);
+                }, 100);
+              }}>
+              {weeks.map((dates, index) => (
+                <View
+                  style={[styles.itemRow, { paddingHorizontal: 16 }]}
+                  key={index}>
+                  {dates.map((item, dateIndex) => {
+                    const isActive =
+                      value.toDateString() === item.date.toDateString();
+                    return (
+                      <TouchableWithoutFeedback
+                        key={dateIndex}
+                        onPress={() => setValue(item.date)}>
+                        <View
+                          style={
+                            styles.item}>
+                          <Text
+                            style={[
+                              styles.itemWeekday,
+                              isActive && { color: '#fff' },
+                            ]}>
+                            {item.weekday}
+                          </Text>
+                          <View style={[
+                                styles.itemDateContainer,
+                                isActive && {backgroundColor: '#fff' },
+                              ]}>
+                            <Text
+                              style={[
+                                styles.itemDate,
+                                isActive && {color: '#000' },
+                              ]}>
+                              {item.date.getDate()}
+                            </Text>
+                          </View>
+                        </View>
+                      </TouchableWithoutFeedback>
+                    );
+                  })}
+                </View>
+              ))}
+            </Swiper>
+          </View>
+      </View>
+      <View style={styles.alertsContainer}>
+        <View style={styles.alerts}>
+          <View style={styles.alertsTexts}>
+          <Text style={styles.text_alert}>Não há atividades futuras.</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomButtonsContainer}>
+        <View style={styles.bottomButtons}>
+
+        
+          <TouchableOpacity
+            style={styles.bottomElementsButtons}
+            underlayColor="lightblue"
+            onPress={() => navigation.navigate('Subjects')}
+          >
+              <Ionicons name="library" size={32} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.newActictyButtom}
+            underlayColor="lightblue"
+            onPress={() => navigation.navigate('NewActivity')}
+          >
+              <Text style={styles.newactivity}>Nova Atividade</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.bottomElementsButtons}
+            underlayColor="lightblue"
+            onPress={() => navigation.navigate('Camera')}
+          >
+              <Entypo name="camera" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+  viewp: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#1E1E1E',
+  },
+
+  calendar_text: {
+    color: 'white',
+    fontFamily:"Poppins-Medium",
+    fontSize: 16,
+  },
+
+  calendar_button: {
+    marginTop: 30,
+    width: "100%",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  placeholderInset: {
+    borderWidth: 4,
+    borderColor: '#e5e7eb',
+    borderStyle: 'dashed',
+    borderRadius: 9,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: '#007aff',
+    borderColor: '#007aff',
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  swiperContainer: {
+    paddingVertical: 15,
+  },
+  picker: {
+    height: 100,
+    maxHeight: 120,
+    paddingVertical: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitle: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#999999',
+    marginBottom: 12,
+  },
+  header: {
+    paddingHorizontal: 16,
+  },
+
+  content: {
+    paddingHorizontal: 16,
+  },
+
+  footer: {
+    marginTop: 'auto',
+    paddingHorizontal: 16,
+  },
+
+  itemRow: {
+    width: width,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    marginHorizontal: -4,
+  
+  },
+
+  item: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 50,
+    
+  },
+  
+  itemWeekday: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#297447',
+    marginBottom: 4,
+    fontFamily: 'Poppins-Bold',
+  },
+
+  itemDate: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: 'white',
+    
+  },
+
+  itemDateContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#262626',
+    height: 42,
+    width: 42,
+    borderRadius: 50,
+  },
+
+  itemtoday: {
+    marginTop: 3,
+    fontSize: 14,
+    fontFamily: 'Poppins-Regular',
+    display: 'none',
+  },
+
+  alertsContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+
+
+  alerts: {
+    backgroundColor: '#C95555',
+    width: '80%',
+    height: 200,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  alertsTexts: {
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text_alert: {
+    fontSize: 24,
+    fontFamily: 'Poppins-Bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  bottomButtons: {
+    width: '80%', 
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  bottomButtonsContainer: {
+    width: '100%', 
+    position: 'absolute',
+    bottom: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  bottomElementsButtons: {
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#297447',
+    borderRadius: 50
+  },
+
+  newactivity: {
+    fontFamily: 'Poppins-Bold',
+    color: 'white',
+    fontSize: 16,
+  },
+
+  newActictyButtom: {
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#297447',
+    borderRadius: 50,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+
+});
